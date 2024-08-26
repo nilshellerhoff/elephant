a7c10fe2a9a995bd359d6391cf1cb151e19429eb
nilshellerhoff <nils.hellerhoff@gmail.com>
2024-08-26 22:56:50 +0200

    fix external preview urls

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,9 +1,12 @@
 import axios, { AxiosResponse } from 'axios';
 
-export const apiClient = axios.create({
-});
+export const apiClient = axios.create({});
 
-export const redditGetResponse = <T>(url: string): Promise<AxiosResponse<T>> => {
-  const proxiedUrl = `https://corsproxy.io/?${encodeURIComponent(url)}`
-  return axios.get(proxiedUrl)
-}
+export const redditGetResponse = <T>(
+  url: string
+): Promise<AxiosResponse<T>> => {
+  const urlObj = new URL(url);
+  urlObj.searchParams.set('raw_json', 1);
+  const proxiedUrl = `https://corsproxy.io/?${encodeURIComponent(urlObj)}`;
+  return axios.get(proxiedUrl);
+};
