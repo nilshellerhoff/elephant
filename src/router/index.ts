f87c9a59498c693e20a56641fb83f41e7ddaa97b
nilshellerhoff <nils.hellerhoff@gmail.com>
2024-08-27 18:10:17 +0200

    restore saved scroll on back

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,10 +21,16 @@ export default route(function (/* { store, ssrContext } */) {
   // const createHistory = process.env.SERVER
   //   ? createMemoryHistory
   //   : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
-  const createHistory = createWebHistory
+  const createHistory = createWebHistory;
 
   const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition;
+      } else {
+        return { top: 0 };
+      }
+    },
     routes,
 
     // Leave this as is and make changes in quasar.conf.js instead!
