0d5c8e7b3898a9deca5c5ed452964addd38fedb3
nilshellerhoff <nils.hellerhoff@gmail.com>
2024-08-27 23:16:37 +0200

    add rudimentary search

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,3 +1,10 @@
 <template>
-  <q-input></q-input>
+  <q-input v-model="input"></q-input>
+  <q-btn v-if="input" :label="`go to r/${input}`" :to="`/r/${input}`" />
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const input = ref('');
+</script>
