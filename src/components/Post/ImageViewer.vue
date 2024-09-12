0066c66f937e11c76207c8244977034eb0b2c8ed
nilshellerhoff <nils.hellerhoff@gmail.com>
2024-09-12 18:23:23 +0200

    zoomable image viewer

--- src/components/Post/ImageViewer.vue
+++ src/components/Post/ImageViewer.vue
@@ -4,22 +4,58 @@
     @hide="onDialogHide"
     maximized
     style="background-color: rgba(0, 0, 0, 0.6)"
-    v-touch-swipe.down="onDialogOK"
   >
-    <q-img fit="scale-down" :src="url" />
+    <div
+      style="
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 60px;
+        background: linear-gradient(
+          0deg,
+          rgba(0, 0, 0, 0) 0%,
+          rgba(0, 0, 0, 0.2) 50%,
+          rgba(0, 0, 0, 0.6) 100%
+        );
+        z-index: 1000;
+      "
+    >
+      <q-btn
+        icon="close"
+        style="color: #fff; float: right"
+        size="20px"
+        @click="onDialogOK"
+        flat
+      />
+      <q-btn
+        icon="fit_screen"
+        style="color: #fff; float: right"
+        size="20px"
+        @click="reset"
+        flat
+      />
+    </div>
+    <ImageViewerZoomable ref="zoomable" :url="url" @close="onDialogOK" />
   </q-dialog>
 </template>
 
 <script setup lang="ts">
 import { useDialogPluginComponent } from 'quasar';
+import ImageViewerZoomable from 'components/Post/ImageViewerZoomable.vue';
+import { ref } from 'vue';
 
 interface Props {
   url: string;
 }
-
 defineProps<Props>();
-
 defineEmits([...useDialogPluginComponent.emits]);
+
+const zoomable = ref();
+
+const reset = () => {
+  zoomable.value.reset();
+};
 
 const { dialogRef, onDialogHide, onDialogOK } = useDialogPluginComponent();
 </script>
---
+++ src/components/Post/ImageViewerZoomable.vue
@@ -0,0 +1,117 @@
+<template>
+  <PinchScrollZoom
+    ref="zoomer"
+    within
+    centered
+    key-actions
+    :min-scale="1"
+    :max-scale="6"
+    :width="width"
+    :height="height"
+    @scaling="(e) => onEvent('scaling', e)"
+    @start-drag="(e) => onEvent('startDrag', e)"
+    @stop-drag="(e) => onEvent('stopDrag', e)"
+    @dragging="(e) => onEvent('dragging', e)"
+    style="overflow: hidden"
+    v-touch-swipe.down="onSwipeDown"
+  >
+    <img :ref="imageElement" :src="url" draggable="false" />
+  </PinchScrollZoom>
+  <div
+    style="
+      position: fixed;
+      width: 100vh;
+      height: 100vh;
+      pointer-events: none;
+      color: #ff0000;
+      font-size: 16px;
+    "
+    v-if="false"
+    v-html="logMessagesHtml"
+  ></div>
+</template>
+
+<script setup lang="ts">
+import '@coddicat/vue-pinch-scroll-zoom/style.css';
+import { ref, computed, Ref } from 'vue';
+import PinchScrollZoom, {
+  type PinchScrollZoomEmitData,
+  type PinchScrollZoomExposed,
+} from '@coddicat/vue-pinch-scroll-zoom';
+import { TouchSwipeValue } from 'quasar';
+
+interface Props {
+  url: string;
+}
+defineProps<Props>();
+const emit = defineEmits(['close']);
+
+const zoomer = ref<PinchScrollZoomExposed>();
+const imageElement = ref();
+
+const zoomState: PinchScrollZoomEmitData = {
+  x: 0,
+  y: 0,
+  scale: 0,
+  originX: 0,
+  originY: 0,
+  translateX: 0,
+  translateY: 0,
+};
+
+const width = computed(() => window.innerWidth);
+const height = computed(() => window.innerHeight);
+
+const onEvent = (name: string, e: PinchScrollZoomEmitData): void => {
+  addLog(name);
+  zoomState.scale = e.scale;
+  zoomState.originX = e.originX;
+  zoomState.originY = e.originY;
+  zoomState.translateX = e.translateX;
+  zoomState.translateY = e.translateY;
+};
+
+const onSwipeDown: TouchSwipeValue = (e) => {
+  console.log('swipe down');
+  // wait for 100ms before emitting close in case swiping was part of zooming in event
+  setTimeout(() => {
+    if (zoomState.scale === 1) {
+      addLog('swipeDown');
+      // when only listening for touches we get a TouchEvent for sure
+      const touches = (e.evt as TouchEvent).touches;
+      // only emit if only one finger
+      if (touches.length == 1) emit('close');
+    }
+  }, 100);
+};
+
+const reset = (): void => {
+  zoomer.value?.setData({
+    scale: 1,
+    originX: 150,
+    originY: 200,
+    translateX: -100,
+    translateY: -50,
+  });
+};
+
+const logMessages: Ref<string[]> = ref([]);
+const addLog = (message: string) => {
+  logMessages.value.push(message);
+  if (logMessages.value.length > 25) logMessages.value.shift();
+};
+const logMessagesHtml = computed(() => logMessages.value.join('<br>'));
+
+defineExpose({ reset });
+</script>
+
+<style scoped>
+img {
+  max-width: 100%;
+  max-height: 100%;
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
